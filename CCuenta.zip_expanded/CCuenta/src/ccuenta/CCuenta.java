1dedb66e6a6cb8b60c2e8a86afd843a9434906d6
ana belen <ana belen@DESKTOP-OCCNPK9.home>
2020-03-23 22:19:01 +0100

    Segunda versión. Refactorizado con método operacuenta y parámetro y encapsulada CCuenta

--- CCuenta.zip_expanded/CCuenta/src/ccuenta/CCuenta.java
+++ CCuenta.zip_expanded/CCuenta/src/ccuenta/CCuenta.java
@@ -26,27 +26,27 @@ public class CCuenta {
     // Constructor con parámetro para iniciar todas las propiedades de la clase
     public CCuenta (String nom, String cue, double sal, double tipo)
     {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
-        tipoInteres=tipo;
+        setNombre(nom);
+        setCuenta(cue);
+        setSaldo(sal);
+        setTipoInteres(tipo);
     }
    // Método para asignar el nombre del titular de la cuenta
     public void asignarNombre(String nom)
     {
-        nombre=nom;
+        setNombre(nom);
     }
     // Método que me devuelve el nombre del titular
     public String obtenerNombre()
     {
 
-            return nombre;
+            return getNombre();
     }
 
     // Método que me devuelve el saldo disponible en cada momento
      public double estado ()
     {
-        return saldo;
+        return getSaldo();
     }
 
     /* Método para ingresar cantidades en la cuenta. Modifica el saldo.
@@ -56,7 +56,7 @@ public class CCuenta {
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
-        saldo = saldo + cantidad;
+        setSaldo(getSaldo() + cantidad);
     }
 
 
@@ -70,12 +70,36 @@ public class CCuenta {
             throw new Exception ("No se puede retirar una cantidad negativa");
         if (estado()< cantidad)
             throw new Exception ("No se hay suficiente saldo");
-        saldo = saldo - cantidad;
+        setSaldo(getSaldo() - cantidad);
     }
 
     // Método que me devuelve el número de cuenta
     public String obtenerCuenta ()
     {
-        return cuenta;
+        return getCuenta();
     }
+private String getNombre() {
+	return nombre;
+}
+private void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+private String getCuenta() {
+	return cuenta;
+}
+private void setCuenta(String cuenta) {
+	this.cuenta = cuenta;
+}
+private double getSaldo() {
+	return saldo;
+}
+private void setSaldo(double saldo) {
+	this.saldo = saldo;
+}
+private double getTipoInteres() {
+	return tipoInteres;
+}
+private void setTipoInteres(double tipoInteres) {
+	this.tipoInteres = tipoInteres;
+}
 }
